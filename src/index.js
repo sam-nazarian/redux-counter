9776a2d8d8c7a92ffbcbb988337e51c8178e9691
samanfathnazarian <saman.nazarian17@gmail.com>
2023-02-13 07:59:50 -0600

    Providing the Store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; //Provider is a component
 
 import './index.css';
 import App from './App';
+import store from './store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  // App component & it's child components will get access to the store
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
